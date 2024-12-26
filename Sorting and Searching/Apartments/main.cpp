8667e7367fc258899d408820979f48aefa2de79c
Nack34 <gmnack9@gmail.com>
2024-12-26 16:02:15 -0300

    Apartments v1 sin el erase - ACCEPTED

--- Sorting and Searching/Apartments/main.cpp
+++ Sorting and Searching/Apartments/main.cpp
@@ -13,6 +13,7 @@ int main() {
       if (cin.get(c) && c == '\n') break;
 
    }
+   sort(desired_sizes.begin(),desired_sizes.end());
    
    vector<long long> v;
    while (cin >> ab){
@@ -23,27 +24,24 @@ int main() {
    sort(v.begin(),v.end());
 
    long long cant = 0;
+   auto i = size_t(0);
+
    for (long long elem : desired_sizes){
+      if (i >= v.size()) break;
+      
       long long desired = elem;
       long long min = desired - k;
       long long max = desired + k;
          
-      auto i = size_t(0);
       for (size_t b = v.size() / 2; b >= 1; b /= 2) {
-         while (true){
-            while (i + b < v.size() && v[i + b] !=-1 && v[i + b] < min) i += b;
-            
-            if (i < v.size() && v[i+1] < min && v[i + b] ==-1) i++;
-            else break;
- 
-         }
+         while (i + b < v.size() && v[i + b] < min) i += b;
       }
       if (i < v.size() && v[i] < min){
          i++;
       }
       if (i < v.size() && (v[i] >= min && v[i] <= max)) {
          cant++;
-         v[i] = -1;
+         i++;
       }
 
    }
