db6ea593fe762ea1918af9055a9a19d4850c4f28
Nack34 <gmnack9@gmail.com>
2024-12-29 15:49:58 -0300

    Ferris Wheel v1

---
+++ Sorting and Searching/Ferris Wheel/main.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main() {
+   char c;
+   long long n, x, p;
+   cin >> n >> x;
+   long long res = 0;
+   
+   vector<long long> cant_pesos(200000, 0);
+   long long max_peso = 0;
+   while (cin >> p){
+      max_peso = p>max_peso ? p : max_peso;
+      cant_pesos[p]++;
+      if (cin.get(c) && c == '\n') break;
+   }
+ 
+   for (long long i=max_peso; i>0; i--){
+      if (cant_pesos[i] != 0){
+         if (i < x){
+            long long j = x-i;
+            
+            while (j>0 && cant_pesos[i] != 0){
+               if (cant_pesos[j]>0){
+                  res+=cant_pesos[j];
+                  cant_pesos[i]-=cant_pesos[j];
+                  cant_pesos[j]=0;
+               }
+               j--;
+            }
+         }
+         res+=cant_pesos[i];
+         cant_pesos[i]=0;
+      }
+   }
+ 
+   cout << res;
+ 
+   return 0;
+}
