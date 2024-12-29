732e0b0b621a06797ec98af5a89b1e54fcbfae5b
Nack34 <gmnack9@gmail.com>
2024-12-29 18:05:10 -0300

    v1 qwq

--- Sorting and Searching/Ferris Wheel/main.cpp
+++ Sorting and Searching/Ferris Wheel/main.cpp
@@ -1,55 +1,37 @@
 #include <bits/stdc++.h>
-
+ 
 using namespace std;
-
+ 
 int main() {
    char c;
-   long long n, x, p;
+   long long n, x, p, max_p, min_p;
    cin >> n >> x;
    long long res = 0;
    
-   map<long long, long long, greater<long long>> cant_pesos;
-
-   while (cin >> p) {
-      cant_pesos[p]++;
+   vector<long long> pesos;
+   while (cin >> p){
+      pesos.push_back(p);
       if (cin.get(c) && c == '\n') break;
    }
+   sort(pesos.begin(),pesos.end());
 
-   for (auto it = cant_pesos.begin(); it != cant_pesos.end(); ++it) {
-      long long peso = it->first;
-
-      if (it->second > 0) {
-         if (peso < x) {
-            long long complement = x - peso < peso ? x - peso : peso;
-            auto jt = cant_pesos.find(complement);
-            if (jt == cant_pesos.end()) {
-               cant_pesos[complement]=0;
-               jt = cant_pesos.find(complement);
-            }
-            
-            if (jt->first == peso && jt->first+peso<=x){
-               res+=it->second/2;
-               it->second=it->second%2;
-               jt++;
-            }
-
-            while (jt != cant_pesos.end() && it->second !=0) {
-               if (jt->second>0){
-                  long long cant_pares_logrados = min(it->second, jt->second);
-                  res += cant_pares_logrados;
-                  it->second -= cant_pares_logrados;
-                  jt->second -= cant_pares_logrados;
-               }
-               jt++;
-            }
-         }
-
-         res += it->second;
-         it->second = 0;
+   max_p=pesos.size()-1;
+   min_p=0;
+   while (true){
+      if (max_p == min_p){
+         res++;
+         break;
       }
+      else if (max_p < min_p) break;
+
+      if (pesos[max_p] + pesos[min_p] <= x){
+         min_p++;
+      }
+      max_p--;
+      res++;
    }
-
+ 
    cout << res;
-
+ 
    return 0;
 }
