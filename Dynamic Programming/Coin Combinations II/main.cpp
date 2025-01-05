37e8e512eb2323b1afbcd3e54f4e7a2f72e3267b
Nack34 <gmnack9@gmail.com>
2025-01-04 21:38:50 -0300

    Coin Combinations II - v2

--- Dynamic Programming/Coin Combinations II/main.cpp
+++ Dynamic Programming/Coin Combinations II/main.cpp
@@ -4,38 +4,41 @@ using namespace std;
  
 int main() {
    char c;
-   long long n, x, v;
+   long long n, x, v, cant;
    cin >> n >> x;
 
    set<long long> values;
-   vector<long long> cants(x+1, 0);
+   vector<map<long long,long long>> cants(x+1);
 
    while (cin >> v){
       values.insert(v);
       if (cin.get(c) && c == '\n') break;
    }
 
-   cants[0]=0;
    for (long long i=1; i<x+1; ++i){
       auto it = values.upper_bound(i);
       while(it != values.begin()) {
          it--;
          v = *it;
          if (i-v == 0){
-            cants[i]++;
-         } else if (i-v > 0 and cants[i-v]>0){
-            cants[i]+=cants[i-v];
+            cants[i][v]=1;
+         } else if (i-v > 0){
+            auto siguiente = cants[i-v].upper_bound(v);
+
+            long long suma = accumulate(cants[i-v].begin(), siguiente, 0,[](long long acc, const pair<long long,long long>& p) {
+                                                      return (acc + p.second)% 1000000007;
+                                                   });
+            if (suma>0){
+               cants[i][v]=suma;
+            }
          }
-         cants[i] = cants[i]% 1000000007;
       }
    }
    
-   cout << cants[x];
-   
-   cout << "\n";
-   for (long long i=0; i<x+1; ++i){
-      cout << i << ": " << cants[i] << '\n';
-   }
+   long long res = accumulate(cants[x].begin(), cants[x].end(), 0,[](int acc, const pair<int,int>& p) {
+                                             return acc + p.second;
+                                          });
+   cout << res;
 
    return 0;
 }
