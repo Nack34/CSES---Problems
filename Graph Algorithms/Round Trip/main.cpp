32324e2f61a57268a99dac2fe1a50b02579fecd8
Nack34 <gmnack9@gmail.com>
2025-01-12 18:03:20 -0300

    por que este es mas rapido? qwq

--- Graph Algorithms/Round Trip/main.cpp
+++ Graph Algorithms/Round Trip/main.cpp
@@ -1,69 +1,64 @@
 #include <bits/stdc++.h>
- 
+
 using namespace std;
-  
+
 int main() {
-   int n, m, a, b;
-   cin >> n >> m;
+    int n, m, a, b;
+    cin >> n >> m;
 
-   stack<tuple<int,int,int>> s;  // Usamos una pila en lugar de una cola para DFS
-   vector<bool> visited(n, false);
-   vector<int> anterior(n, -1);
-   vector<int> n_steps(n, -1);
+    stack<tuple<int, int>> s;  // Usamos una pila en lugar de una cola para DFS
+    vector<bool> visited(n, false);
+    vector<int> padre(n, -1);  // Para rastrear el padre de cada nodo
+    vector<int> n_steps(n, -1);
 
-   vector<vector<int>> vecinos(n);
-   for (int i=0; i<m; ++i){
-      cin >> a >> b;
-      vecinos[a-1].push_back(b-1);
-      vecinos[b-1].push_back(a-1);
-   }
+    vector<vector<int>> vecinos(n);
+    for (int i = 0; i < m; ++i) {
+        cin >> a >> b;
+        vecinos[a - 1].push_back(b - 1);
+        vecinos[b - 1].push_back(a - 1);
+    }
 
-   for (int i=0; i<n;++i){
-      if (visited[i]) continue;
-      
-      s.push({i,-1, 0});
-      while (!s.empty()) {
-         auto [j,w,n_step] = s.top(); s.pop();
-         if (visited[j]) continue;
+    for (int i = 0; i < n; ++i) {
+        if (visited[i]) continue;
 
-         //cout << "Nodo actual: " << j+1 << "\n";
-         visited[j] = true;
-         n_steps[j] = n_step+1;
-         anterior[j]=w;
+        s.push({i, 0});
+        while (!s.empty()) {
+            auto [j, n_step] = s.top();
+            s.pop();
+            if (visited[j]) continue;
 
-         for (auto v:vecinos[j]) {
-            if (visited[v] && n_steps[j]-n_steps[v]>=2) {
+            visited[j] = true;
+            n_steps[j] = n_step;
 
-               anterior[v] = j;
-               string camino = to_string(v+1)+" ";
+            for (auto v : vecinos[j]) {
+                if (!visited[v]) {
+                    padre[v] = j;
+                    s.push({v, n_step + 1});
+                } else if (padre[j] != v && n_steps[j] - n_steps[v] >= 2) {
+                    // Encontramos un ciclo de longitud al menos 3
 
-               int cant = 2;
-               int actual = j;
-               camino = to_string(actual+1) + " " + camino;
-               while (anterior[actual] != j){
-                  actual = anterior[actual];
-                  ++cant;
-                  camino = to_string(actual+1) + " " + camino;
-               }
+                    // Reconstruir el camino desde j hasta v
+                    vector<int> ciclo;
+                    int current = j;
+                    while (current != v) {
+                        ciclo.push_back(current + 1);
+                        current = padre[current];
+                    }
+                    ciclo.push_back(v + 1);
 
-               cout << cant << "\n" << camino;
-               return 0;
-
+                    // Imprimir el ciclo
+                    cout << ciclo.size()+1 << "\n";
+                    for (int k = ciclo.size() - 1; k >= 0; --k) {
+                        cout << ciclo[k] << ' ';
+                    }
+                    cout << v+1 << ' ';
+                    cout << "\n";
+                    return 0;
+                }
             }
-            s.push({v,j,n_steps[j]}); 
-         }
-      }
-   }
-   
-   /*for (int i=0; i<n;++i){
-      cout << n_steps[i] << ' ';
-   }
-   cout << "\n";
-   for (int i=0; i<n;++i){
-      cout << anterior[i] << ' ';
-   }
-   cout << "\n";*/
+        }
+    }
 
-   cout << "IMPOSSIBLE";
-   return 0;
+    cout << "IMPOSSIBLE";
+    return 0;
 }
