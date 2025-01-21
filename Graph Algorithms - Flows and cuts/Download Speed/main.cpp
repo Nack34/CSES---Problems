e83461c7df8c69a409065407cebc316f21b65f2d
Nack34 <gmnack9@gmail.com>
2025-01-21 18:01:14 -0300

    ACCEPTED con DFS

--- Graph Algorithms - Flows and cuts/Download Speed/main.cpp
+++ Graph Algorithms - Flows and cuts/Download Speed/main.cpp
@@ -3,6 +3,56 @@
 using namespace std;
 using namespace std::chrono;
 
+
+tuple<long long, deque<int>> augmentation_path(unordered_map<int, unordered_map<int, long long>>& adj, int s, int t, int n){  
+   int a, p;
+   long long cur_c_max;
+   bool terminated = false;
+   long long c_max;
+   vector<bool> visited(n, false);
+   vector<int> prev(n, -1);
+   stack<tuple<int, long long, int>> stk; // current node, cur_c_max (current capacidad max del camino), previo
+   stk.push({s, LONG_LONG_MAX, -1});
+   while (!stk.empty()){
+      tie(a, cur_c_max, p) = stk.top(); stk.pop();
+      if (visited[a]) continue;
+      visited[a]=true;
+      prev[a]=p;
+
+      if (a == t) {
+         c_max = cur_c_max;
+         terminated = true;
+         break;
+      }
+      //cout << "current a: "<< a+1 << ", cur_c_max: " << cur_c_max << "\n";
+
+      for (const auto& [b, cr]:adj[a]){ // destino, capacidad restante
+         //cout << "vecino: "<< b+1 << ", cr: " << cr << "\n";
+         if (!visited[b] && cr>0){
+            //cout << "anotando vecino "<< b+1 << "\n";
+            stk.push({b, min(cur_c_max, cr), a});
+         }
+      }
+   }
+
+   deque<int> path;
+   if (!terminated) return {-1, path};
+
+   int b = t;
+   p = prev[b];
+   path.push_front(b+1);
+   while (b != s){
+      adj[p][b]-=c_max;
+      adj[b][p]+=c_max;
+      b = p;
+      p = prev[b];
+      path.push_front(b+1);
+   }
+   //cout << "current max in function: " << c_max << "\n";
+
+   return {c_max, path}; 
+}
+
 tuple<long long, deque<int>> min_augmentation_path(unordered_map<int, unordered_map<int, long long>>& adj, int s, int t, int n){  
    int a;
    long long cur_c_max;
@@ -54,12 +104,12 @@ int main() {
    auto start = high_resolution_clock::now();  // Marca el inicio
 
    // Abrir archivo de entrada y salida
-   ifstream input("input.txt");  // Este archivo simula la entrada estándar (cin)
-   ofstream output("output.txt"); // Este archivo simula la salida estándar (cout)
+   //ifstream input("input.txt");  // Este archivo simula la entrada estándar (cin)
+   //ofstream output("output.txt"); // Este archivo simula la salida estándar (cout)
 
    // Redirigir cin y cout a los archivos
-   cin.rdbuf(input.rdbuf());
-   cout.rdbuf(output.rdbuf());
+   //cin.rdbuf(input.rdbuf());
+   //cout.rdbuf(output.rdbuf());
 
 
    int n, m, a, b, c;
@@ -85,7 +135,7 @@ int main() {
    long long flow = 0;
    long long c_max;
    deque<int> path;
-   tie(c_max, path) = min_augmentation_path(adj, s, t, n);
+   tie(c_max, path) = augmentation_path(adj, s, t, n);
    while (c_max!=-1 && path.size() != 0){
       /*cout << "path:  ";
 
@@ -102,13 +152,13 @@ int main() {
       //cin >> pausar;
 
 
-      tie(c_max, path) = min_augmentation_path(adj, s, t, n);
+      tie(c_max, path) = augmentation_path(adj, s, t, n);
    }
    cout << flow;
    auto end = high_resolution_clock::now();    // Marca el final
    auto duration = duration_cast<milliseconds>(end - start);
-   cout << "\n";
-   cout << "Tiempo de ejecución: " << duration.count() << " ms (" << duration.count()/1000 << " s)" << endl;
+   //cout << "\n";
+   //cout << "Tiempo de ejecución: " << duration.count() << " ms (" << duration.count()/1000 << " s)" << endl;
    
    return 0;
 }
